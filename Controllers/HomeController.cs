0abd3742dc4c1719c953ff1ad1962743a2df807c
Nabeel <inabeel@live.com>
2016-10-30 00:10:13 +0500

    Home controller checked

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,38 +6,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Zoltu.Bags.Client.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            ViewData["url"] = "https://bagcupid.com/";
-            ViewData["type"] = "website";
-            ViewData["title"] = "Bag Cupid";
-            ViewData["description"] = "What is your dream bag? Are you having trouble finding it? Let us help you!";
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			ViewData["url"] = "https://bagcupid.com/";
+			ViewData["type"] = "website";
+			ViewData["title"] = "Bag Cupid";
+			ViewData["description"] = "What is your dream bag? Are you having trouble finding it? Let us help you!";
 
-            return View();
-        }
+			return View();
+		}
 
-        [Route("app", Name = "app", Order = 0)]
-        public IActionResult App()
-        {
-            ViewData["url"] = "https://bagcupid.com/";
-            ViewData["type"] = "website";
-            ViewData["title"] = "Bag Cupid";
-            ViewData["description"] = "What is your dream bag? Are you having trouble finding it? Let us help you!";
+		[Route("app", Name = "app", Order = 0)]
+		public IActionResult App()
+		{
+			ViewData["url"] = "https://bagcupid.com/";
+			ViewData["type"] = "website";
+			ViewData["title"] = "Bag Cupid";
+			ViewData["description"] = "What is your dream bag? Are you having trouble finding it? Let us help you!";
 
-            return View("Index");
-        }
+			return View("Index");
+		}
 
-        [Route("app/product/{productId}", Name = "product", Order = 0)]
-        public IActionResult Product(int productId)
-        {
-            ViewData["url"] = "Muhammad Nabeel";
-            ViewData["type"] = "Managing Director";
-            ViewData["title"] = "Principal Software Engineer / Technical Lead";
-            ViewData["description"] = "Relliks Systems";
+		[Route("app/product/{productId}", Name = "product", Order = 0)]
+		public IActionResult Product(int productId)
+		{
+			ViewData["url"] = "Muhammad Nabeel";
+			ViewData["type"] = "Managing Director";
+			ViewData["title"] = "Principal Software Engineer / Technical Lead";
+			ViewData["description"] = "Relliks Systems";
 
-            return View("Index");
-        }
-    }
+			return View("Index");
+		}
+	}
 }
